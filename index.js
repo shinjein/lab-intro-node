059ce3820d72ab4c1be2f59c5723cc25d09af93e
shinjein <shinjein@hu-berlin.de>
2021-04-12 14:38:28 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
 
-  add(item) {}
+  }
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((item1, item2) => {
+      return item1 - item2;
+    });
+      this.length = this.items.length;
+  }
 
-  max() {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+      throw Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  min() {}
+  max() {
+    if(this.items.length == 0) {
+      throw Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items) 
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.items.length == 0) {
+      throw Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  avg() {}
-}
+  sum() {
+     if (this.items.length === 0) {
+      return 0;
+    }
+    let total = 0;
+    for(let i = 0; i < this.items.length; i++) {
+      total += this.items[i];
+      } return total; 
+    }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw Error("EmptySortedList");
+    } 
+    let someSum = this.sum(this.items);
+    let someAverage = someSum / this.length;
+    return someAverage;
+  }
+} 
 
 module.exports = SortedList;
